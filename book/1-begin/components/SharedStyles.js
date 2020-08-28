fad0d3194a0bab4d90a55bb70338e08f66824c9a
Igor Tunde <igortunde@yahoo.com>
2020-08-28 19:00:25 +0400

    type: FEAT
    body: Header component added in the build up to create
          a static login-in page. Also, dotenv package
          (as dependencies in BUILDERBOOK FOLDER) was installed to run
          "yarn dev" - to start dev server.

---
+++ book/1-begin/components/SharedStyles.js
@@ -0,0 +1,9 @@
+const styleToolbar = {
+  background: '#FFF',
+  height: '64px',
+  paddingRight: '20px',
+};
+
+module.exports = {
+  styleToolbar,
+};
--- book/1-begin/pages/index.js
+++ book/1-begin/pages/index.js
@@ -1,4 +1,15 @@
-[1, 2, 3].map((x) => {
-  const y = x + 1;
-  return x * y;
-});
+import Head from 'next/head';
+import Header from '../components/Header';
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>Index page</title>
+      <meta name="description" content="This is the description of the Index page" />
+    </Head>
+    <Header />
+    <p>Content on Index page</p>
+  </div>
+);
+
+export default Index;
