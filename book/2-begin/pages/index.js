39f2c2f3b6cc00f352e3aa2e99c3383305e9a9ff
Igor Tunde <igortunde@yahoo.com>
2020-09-07 11:21:33 +0400

    feats: index page populated
    body: index page gets getInitialProps() that populates user object to
          display user's info

--- book/2-begin/pages/index.js
+++ book/2-begin/pages/index.js
@@ -1,13 +1,31 @@
+import PropTypes from 'prop-types';
+
 import Head from 'next/head';
 
-const Index = () => (
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>Index page</title>
-      <meta name="description" content="This is SEO description of Index page" />
+      <meta name="description" content="This is the description of the Index page" />
     </Head>
     <p>Content on Index page</p>
+    <p>
+      Email:
+      {user.email}
+    </p>
   </div>
 );
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+
+Index.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+Index.defaultProps = {
+  user: null,
+};
 
 export default Index;
