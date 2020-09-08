e7992542b989d97912e46daa5a3ce2e43ba5cc41
Igor Tunde <igortunde@yahoo.com>
2020-09-08 12:17:03 +0400

    feat: connected Model and MongoDB Atlas to       the app with mongoose
    created a model/Schema connected to the server with mongoose
    and made a cluster on MongoDB cloud connected to the app
    (dotenv/.env used)

--- book/2-begin/server/app.js
+++ book/2-begin/server/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGO_URL, options);
 
 const port = process.env.PORT || 8000;
 const ROOT_URL = `http://localhost:${port}`;
 
-const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
