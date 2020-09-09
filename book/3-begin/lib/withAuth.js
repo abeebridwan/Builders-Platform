0eb7e4446f7159f1ccb98eae9a3e872d741de380
Igor Tunde <igortunde@yahoo.com>
2020-09-09 13:28:03 +0400

    feat: Parameters added for withAuth HOC
    boolean parameters: loginRequired and logoutRequired added to determine
    which page to serve index page or login page(yet to be created)

--- book/3-begin/lib/withAuth.js
+++ book/3-begin/lib/withAuth.js
@@ -1,10 +1,11 @@
 /* eslint-disable react/static-property-placement */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 
 let globalUser = null;
 
-function withAuth(BaseComponent) {
+function withAuth(BaseComponent, { loginRequired = true, logoutRequired = false } = {}) {
   class App extends React.Component {
     static propTypes = {
       user: PropTypes.shape({
@@ -24,6 +25,17 @@ function withAuth(BaseComponent) {
       if (isFromServer) {
         globalUser = user;
       }
+
+      // If login is required and not logged in, redirect to "/login" page
+      if (loginRequired && !logoutRequired && !user) {
+        Router.push('/login');
+        return;
+      }
+
+      // If logout is required and user logged in, redirect to "/" page
+      if (logoutRequired && user) {
+        Router.push('/');
+      }
     }
 
     static async getInitialProps(ctx) {
@@ -31,12 +43,14 @@ function withAuth(BaseComponent) {
       const user = ctx.req ? ctx.req.user && ctx.req.user.toObject() : globalUser;
 
       if (isFromServer && user) {
+        // Convert "_id"(ObjectID from MongoDB) object to string
         user._id = user._id.toString();
       }
 
       const props = { user, isFromServer };
 
       // Call child component's "getInitialProps", if it is defined
+
       if (BaseComponent.getInitialProps) {
         Object.assign(props, (await BaseComponent.getInitialProps(ctx)) || {});
       }
@@ -45,6 +59,16 @@ function withAuth(BaseComponent) {
     }
 
     render() {
+      const { user } = this.props;
+
+      if (loginRequired && !logoutRequired && !user) {
+        return null;
+      }
+
+      if (logoutRequired && user) {
+        return null;
+      }
+
       return <BaseComponent {...this.props} />;
     }
   }
