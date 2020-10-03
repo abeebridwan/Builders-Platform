cc2b7f141c3f261b6212364e91bc5fbab07ad62a
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2020-10-04 00:14:08 +0100

    feat: sections, sidebar and toggle TOC method added
    TOC - table of content

--- book/7-begin/pages/public/read-chapter.jsx
+++ book/7-begin/pages/public/read-chapter.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable react/sort-comp */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Error from 'next/error';
 import Head from 'next/head';
-
+import Link from 'next/link';
 import { getChapterDetailApiMethod } from '../../lib/api/public';
 import withAuth from '../../lib/withAuth';
+
+const styleIcon = {
+  opacity: '0.75',
+  fontSize: '24px',
+  cursor: 'pointer',
+};
 
 const propTypes = {
   chapter: PropTypes.shape({
@@ -18,8 +26,8 @@ const defaultProps = {
 };
 
 class ReadChapter extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(props, ...args) {
+    super(props, ...args);
 
     const { chapter } = props;
 
@@ -29,6 +37,7 @@ class ReadChapter extends React.Component {
     }
 
     this.state = {
+      showTOC: false,
       chapter,
       htmlContent,
     };
@@ -36,6 +45,8 @@ class ReadChapter extends React.Component {
 
   componentDidUpdate(prevProps) {
     if (prevProps.chapter && prevProps.chapter._id !== this.props.chapter._id) {
+      document.getElementById('chapter-content').scrollIntoView();
+
       const { htmlContent } = prevProps.chapter;
 
       // eslint-disable-next-line
@@ -56,11 +67,14 @@ class ReadChapter extends React.Component {
     return { chapter };
   }
 
+  toggleChapterList = () => {
+    this.setState((prevState) => ({ showTOC: !prevState.showTOC }));
+  };
+
   renderMainContent() {
     const { chapter, htmlContent } = this.state;
 
     return (
-      // eslint-disable-next-line react/jsx-filename-extension
       <div>
         <h2>
           Chapter:
@@ -72,6 +86,70 @@ class ReadChapter extends React.Component {
           // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{ __html: htmlContent }}
         />
+      </div>
+    );
+  }
+
+  renderSections() {
+    const { sections } = this.state.chapter;
+
+    if (!sections || !sections.length === 0) {
+      return null;
+    }
+
+    return (
+      <ul>
+        {sections.map((s) => (
+          <li key={s.escapedText} style={{ paddingTop: '10px' }}>
+            <a href={`#${s.escapedText}`}>{s.text}</a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  renderSidebar() {
+    const { showTOC, chapter } = this.state;
+
+    if (!showTOC) {
+      return null;
+    }
+
+    const { book } = chapter;
+    const { chapters } = book;
+
+    return (
+      <div
+        style={{
+          textAlign: 'left',
+          position: 'absolute',
+          bottom: 0,
+          top: '64px',
+          left: 0,
+          overflowY: 'auto',
+          overflowX: 'hidden',
+          width: '400px',
+          padding: '0px 25px',
+        }}
+      >
+        <p style={{ padding: '0px 40px', fontSize: '17px', fontWeight: '400' }}>{book.name}</p>
+        <ol start="0" style={{ padding: '0 25', fontSize: '14px', fontWeight: '300' }}>
+          {chapters.map((ch, i) => (
+            <li
+              key={ch._id}
+              role="presentation"
+              style={{ listStyle: i === 0 ? 'none' : 'decimal', paddingBottom: '10px' }}
+            >
+              <Link
+                as={`/books/${book.slug}/${ch.slug}`}
+                href={`/public/read-chapter?bookSlug=${book.slug}&chapterSlug=${ch.slug}`}
+              >
+                <a style={{ color: chapter._id === ch._id ? '#1565C0' : '#222' }}>{ch.title}</a>
+              </Link>
+              {chapter._id === ch._id ? this.renderSections() : null}
+            </li>
+          ))}
+        </ol>
       </div>
     );
   }
@@ -96,6 +174,8 @@ class ReadChapter extends React.Component {
           ) : null}
         </Head>
 
+        {this.renderSidebar()}
+
         <div
           style={{
             textAlign: 'left',
@@ -117,6 +197,24 @@ class ReadChapter extends React.Component {
             }}
           />
           {this.renderMainContent()}
+        </div>
+        <div
+          style={{
+            position: 'fixed',
+            // top: hideHeader ? '20px' : '80px',
+            transition: 'top 0.5s ease-in',
+            left: '15px',
+          }}
+        >
+          <i // eslint-disable-line
+            className="material-icons"
+            style={styleIcon}
+            onClick={this.toggleChapterList}
+            onKeyPress={this.toggleChapterList}
+            role="button"
+          >
+            format_list_bulleted
+          </i>
         </div>
       </div>
     );
--- book/7-begin/server/models/Chapter.js
+++ book/7-begin/server/models/Chapter.js
@@ -7,6 +7,99 @@ const generateSlug = require('../utils/slugify');
 
 const { Schema } = mongoose;
 
+function markdownToHtml(content) {
+  const renderer = new marked.Renderer();
+
+  renderer.link = (href, title, text) => {
+    const t = title ? ` title="${title}"` : '';
+    return `<a target="_blank" href="${href}" rel="noopener noreferrer"${t}>${text}</a>`;
+  };
+
+  renderer.image = (href) => `<img
+    src="${href}"
+    style="border: 1px solid #ddd;"
+    width="100%"
+    alt="Builder Book"
+  >`;
+
+  renderer.heading = (text, level) => {
+    const escapedText = text
+      .trim()
+      .toLowerCase()
+      .replace(/[^\w]+/g, '-');
+
+    if (level === 2) {
+      return `<h${level} class="chapter-section" style="color: #222; font-weight: 400;">
+          <a
+            name="${escapedText}"
+            href="#${escapedText}"
+            style="color: #222;"
+          > 
+            <i class="material-icons" style="vertical-align: middle; opacity: 0.5; cursor: pointer;">link</i>
+          </a>
+          <span class="section-anchor" name="${escapedText}">
+            ${text}
+          </span>
+        </h${level}>`;
+    }
+
+    if (level === 4) {
+      return `<h${level} style="color: #222;">
+          <a
+            name="${escapedText}"
+            href="#${escapedText}"
+            style="color: #222;"
+          >
+            <i class="material-icons" style="vertical-align: middle; opacity: 0.5; cursor: pointer;">link</i>
+          </a>
+          ${text}
+        </h${level}>`;
+    }
+
+    return `<h${level} style="color: #222; font-weight: 400;">${text}</h${level}>`;
+  };
+
+  marked.setOptions({
+    renderer,
+    breaks: true,
+    highlight(code, lang) {
+      if (!lang) {
+        return hljs.highlightAuto(code).value;
+      }
+
+      return hljs.highlight(lang, code).value;
+    },
+  });
+
+  return marked(he.decode(content));
+}
+
+function getSections(content) {
+  const renderer = new marked.Renderer();
+
+  const sections = [];
+  renderer.heading = (text, level) => {
+    if (level !== 2) {
+      return;
+    }
+
+    const escapedText = text
+      .trim()
+      .toLowerCase()
+      .replace(/[^\w]+/g, '-');
+
+    sections.push({ text, level, escapedText });
+  };
+
+  marked.setOptions({
+    renderer,
+  });
+
+  marked(he.decode(content));
+
+  return sections;
+}
+
 const mongoSchema = new Schema({
   bookId: {
     type: Schema.Types.ObjectId,
@@ -28,10 +121,6 @@ const mongoSchema = new Schema({
     type: String,
     required: true,
   },
-  excerpt: {
-    type: String,
-    default: '',
-  },
   content: {
     type: String,
     default: '',
@@ -41,6 +130,14 @@ const mongoSchema = new Schema({
     type: String,
     default: '',
     required: true,
+  },
+  excerpt: {
+    type: String,
+    default: '',
+  },
+  htmlExcerpt: {
+    type: String,
+    default: '',
   },
   createdAt: {
     type: Date,
@@ -52,6 +149,13 @@ const mongoSchema = new Schema({
   },
   seoTitle: String,
   seoDescription: String,
+  sections: [
+    {
+      text: String,
+      level: Number,
+      escapedText: String,
+    },
+  ],
 });
 
 class ChapterClass {
@@ -144,100 +248,6 @@ class ChapterClass {
 
     return this.updateOne({ _id: chapter._id }, { $set: modifier });
   }
-}
-
-function markdownToHtml(content) {
-  const renderer = new marked.Renderer();
-
-  renderer.link = (href, title, text) => {
-    const t = title ? ` title="${title}"` : '';
-    return `<a target="_blank" href="${href}" rel="noopener noreferrer"${t}>${text}</a>`;
-  };
-
-  renderer.image = (href) => `<img
-    src="${href}"
-    style="border: 1px solid #ddd;"
-    width="100%"
-    alt="Builder Book"
-  >`;
-
-  renderer.heading = (text, level) => {
-    const escapedText = text
-      .trim()
-      .toLowerCase()
-      .replace(/[^\w]+/g, '-');
-
-    if (level === 2) {
-      return `<h${level} class="chapter-section" style="color: #222; font-weight: 400;">
-        <a
-          name="${escapedText}"
-          href="#${escapedText}"
-          style="color: #222;"
-        > 
-          <i class="material-icons" style="vertical-align: middle; opacity: 0.5; cursor: pointer;">link</i>
-        </a>
-        <span class="section-anchor" name="${escapedText}">
-          ${text}
-        </span>
-      </h${level}>`;
-    }
-
-    if (level === 4) {
-      return `<h${level} style="color: #222;">
-        <a
-          name="${escapedText}"
-          href="#${escapedText}"
-          style="color: #222;"
-        >
-          <i class="material-icons" style="vertical-align: middle; opacity: 0.5; cursor: pointer;">link</i>
-        </a>
-        ${text}
-      </h${level}>`;
-    }
-
-    return `<h${level} style="color: #222; font-weight: 400;">${text}</h${level}>`;
-  };
-
-  marked.setOptions({
-    renderer,
-    breaks: true,
-    highlight(code, lang) {
-      if (!lang) {
-        return hljs.highlightAuto(code).value;
-      }
-
-      return hljs.highlight(lang, code).value;
-    },
-  });
-
-  return marked(he.decode(content));
-}
-
-function getSections(content) {
-  const renderer = new marked.Renderer();
-
-  const sections = [];
-
-  renderer.heading = (text, level) => {
-    if (level !== 2) {
-      return;
-    }
-
-    const escapedText = text
-      .trim()
-      .toLowerCase()
-      .replace(/[^\w]+/g, '-');
-
-    sections.push({ text, level, escapedText });
-  };
-
-  marked.setOptions({
-    renderer,
-  });
-
-  marked(he.decode(content));
-
-  return sections;
 }
 
 mongoSchema.index({ bookId: 1, slug: 1 }, { unique: true });
