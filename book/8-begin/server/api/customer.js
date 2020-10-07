fdf540a24b8fe3b99df06cadd19ff65f8791d064
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2020-10-07 01:55:28 +0100

    feat: Stripe method createSession added

--- book/8-begin/server/api/customer.js
+++ book/8-begin/server/api/customer.js
@@ -4,6 +4,37 @@ const Book = require('../models/Book');
 
 const router = express.Router();
 
+function createSession({ userId, bookId, bookSlug, userEmail, redirectUrl }) {
+  console.log(userId, bookId, bookSlug, userEmail, redirectUrl);
+  return stripeInstance.checkout.sessions.create({
+    customer_email: userEmail,
+    payment_method_types: ['card'],
+    mode: 'payment',
+    line_items: [{ price: getBookPriceId(bookSlug), quantity: 1 }],
+    success_url: `${ROOT_URL}/stripe/checkout-completed/{CHECKOUT_SESSION_ID}`,
+    cancel_url: `${ROOT_URL}${redirectUrl}?checkout_canceled=1`,
+    metadata: { userId, bookId, redirectUrl },
+  });
+}
+
+function getBookPriceId(bookSlug) {
+  let priceId;
+
+  if (bookSlug === 'demo-book') {
+    priceId = dev
+      ? process.env.STRIPE_TEST_DEMO_BOOK_PRICE_ID
+      : process.env.STRIPE_LIVE_DEMO_BOOK_PRICE_ID;
+  } else if (bookSlug === 'second-book') {
+    priceId = dev
+      ? process.env.STRIPE_TEST_SECOND_BOOK_PRICE_ID
+      : process.env.STRIPE_LIVE_SECOND_BOOK_PRICE_ID;
+  } else {
+    throw new Error('Wrong book');
+  }
+
+  return priceId;
+}
+
 router.post('/stripe/fetch-checkout-session', async (req, res) => {
   try {
     const { bookId, redirectUrl } = req.body;
