fc1d38e043580fc1c8c15fd67c74519b273965f4
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2020-10-08 16:11:11 +0100

    feat: Static method Book.buy added to book Model

--- book/8-begin/server/models/Book.js
+++ book/8-begin/server/models/Book.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const frontmatter = require('front-matter');
 const generateSlug = require('../utils/slugify');
 const { getCommits, getRepoDetail } = require('../github');
+const Purchase = require('./Purchase');
+const User = require('./User');
+const { addToMailchimp } = require('../mailchimp');
 
 const { Schema } = mongoose;
 
@@ -146,6 +149,38 @@ class BookClass {
     );
 
     return book.updateOne({ githubLastCommitSha: lastCommitSha });
+  }
+
+  static async buy({ book, user, stripeCharge }) {
+    if (!book) {
+      throw new Error('Book not found');
+    }
+
+    if (!user) {
+      throw new Error('User required');
+    }
+
+    const isPurchased =
+      (await Purchase.find({ userId: user._id, bookId: book._id }).countDocuments()) > 0;
+    if (isPurchased) {
+      throw new Error('You already bought this book.');
+    }
+
+    User.findByIdAndUpdate(user._id, { $addToSet: { purchasedBookIds: book._id } }).exec();
+
+    try {
+      await addToMailchimp({ email: user.email, listName: 'purchased' });
+    } catch (error) {
+      console.error('Buy book error:', error);
+    }
+
+    return Purchase.create({
+      userId: user._id,
+      bookId: book._id,
+      amount: book.price * 100,
+      createdAt: new Date(),
+      stripeCharge,
+    });
   }
 }
 
