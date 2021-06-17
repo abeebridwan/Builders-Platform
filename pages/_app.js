54b4575ad160739b9d20898d16c71e285871d0ee
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-06-17 01:02:23 +0100

    feat: next/materialUI integration rewrited to avoid fouc

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 /* eslint-disable react/jsx-filename-extension */
 import CssBaseline from '@material-ui/core/CssBaseline';
-// import { ThemeProvider, StylesProvider } from '@material-ui/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 import App from 'next/app';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Head from 'next/head';
+// import Head from 'next/head';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
-// import { theme } from '../lib/theme';
+import { theme } from '../lib/theme';
 
 import Notifier from '../components/Notifier';
 import Header from '../components/Header';
@@ -26,10 +26,10 @@ const propTypes = {
 class MyApp extends App {
   componentDidMount() {
     // Remove the server-side injected CSS.
-    /* const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
-    } */
+    }
   }
 
   render() {
@@ -39,15 +39,16 @@ class MyApp extends App {
     // console.log(pageProps.noHeader);
 
     return (
-      <>
-        <Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </Head>
+        </Head> */}
         <CssBaseline />
         {pageProps.chapter ? null : <Header {...pageProps} />}
         <Component {...pageProps} />
         <Notifier />
-      </>
+      </ThemeProvider>
     );
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-// import { ServerStyleSheets } from '@material-ui/styles';
+import { ServerStyleSheets } from '@material-ui/styles';
 
 class MyDocument extends Document {
   /* static getInitialProps = async (ctx) => {   Render app and page and get the context of the page with collected side effects.
@@ -160,4 +160,29 @@ class MyDocument extends Document {
   };
 };
  */
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: (
+      <>
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </>
+    ),
+  };
+};
+
 export default MyDocument;
