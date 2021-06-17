8ed16dd5a7e3e4b23a2038cad23f6cfe4be9a1c4
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2021-06-17 16:59:09 +0100

    fix 03

--- pages/_document.js
+++ pages/_document.js
@@ -6,23 +6,6 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 
 class MyDocument extends Document {
-  /* static getInitialProps = async (ctx) => {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-
-      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-    };
-  }; */
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
